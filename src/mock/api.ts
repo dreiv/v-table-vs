bc6f846878ede115f3850b05f3714c50d802c0f4
Andrei Voicu <dreivoicu@gmail.com>
2021-11-29 15:34:53 +0200

    mock data

---
+++ src/mock/api.ts
@@ -0,0 +1,21 @@
+import data from "./data.json";
+
+const latency = () => Math.random() * 500 + 300; // simulate network latency of 300-800ms
+
+export const API = {
+  load: async (pageNumber = 1, pageSize = 25) =>
+    new Promise((resolve) => {
+      const auxiliary = {};
+      let records = data as any[];
+
+      const response = {
+        ...auxiliary,
+        totalRecords: records.length,
+        pageNumber,
+        pageSize,
+        records,
+      };
+
+      setTimeout(() => resolve(data), latency());
+    }),
+};
