b302353d359f6ecd46eb61ede6dee3056dbd1964
Andrei Voicu <dreivoicu@gmail.com>
2022-02-15 10:07:26 +0200

    cleaning up

--- src/helpers/genId.ts
+++ src/helpers/genId.ts
@@ -1,3 +1,3 @@
 export function genId(): string {
-  return Math.random().toString(36).substr(2);
+  return Math.random().toString(36).substring(2);
 }
--- src/ui/common/components/datatable/index.ts
+++ src/ui/common/components/datatable/index.ts
@@ -1,3 +1,3 @@
-export { default as DataTable } from "./index.vue";
+export { default as DataTable } from "./data-table.vue";
 export * from "./types";
 export * from "./symbols";
