bc23f1a184dce094e6bd5558ec0ba96d1e87cac5
Andrei Voicu <dreivoicu@gmail.com>
2022-02-20 09:04:31 +0200

    improve drag

--- src/ui/common/components/datatable/sub-components/DataTableHeader.vue
+++ src/ui/common/components/datatable/sub-components/DataTableHeader.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
-import { inject } from "vue";
+import { inject, ref } from "vue";
 
 import DataTableHeaderResizeHandle from "./DataTableHeaderResizeHandle.vue";
 import { DataTableKey } from "../symbols";
 
 const context = inject(DataTableKey);
 const COLUMN_KEY = "column-key";
+const targetColumn = ref();
 
 function startDrag(evt: DragEvent, from: string) {
   evt.dataTransfer?.setData(COLUMN_KEY, from);
 }
 
 function onDrop(evt: DragEvent, to: string) {
-  const from = evt.dataTransfer?.getData(COLUMN_KEY) || "";
+  const from = evt.dataTransfer?.getData(COLUMN_KEY) as string;
   context?.value.onSwap(from, to);
+  targetColumn.value = null;
 }
 </script>
 
@@ -24,13 +26,20 @@ function onDrop(evt: DragEvent, to: string) {
       :key="key"
       :class="$style.header"
       @drop="onDrop($event, key)"
-      @dragover.prevent
+      @dragover.prevent="targetColumn = key"
+      @dragleave="targetColumn = null"
     >
-      <div :class="$style.container">
+      <div
+        :class="[
+          { [$style.isDropTarget]: key === targetColumn },
+          $style.container,
+        ]"
+      >
         <div
           :class="$style.title"
           draggable="true"
           @dragstart="startDrag($event, key)"
+          @dragend="targetColumn = null"
         >
           {{ text }}
         </div>
@@ -42,6 +51,8 @@ function onDrop(evt: DragEvent, to: string) {
 </template>
 
 <style lang="scss" module>
+@import "@/ui/assets/styles/abstracts";
+
 .sticky {
   position: sticky;
   top: 0;
@@ -62,5 +73,14 @@ function onDrop(evt: DragEvent, to: string) {
 .title {
   flex: 1;
   cursor: grab;
+}
+
+$viewportHeight: 100vh;
+.isDropTarget::before {
+  @include box(3px, $viewportHeight);
+  content: "";
+
+  box-shadow: 0 $viewportHeight 0 var(--primary);
+  margin-top: -$viewportHeight;
 }
 </style>
